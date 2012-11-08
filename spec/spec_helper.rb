316c3de6dfed16b902ae2cc27566e98f7bf2677e
Dan Sosedoff <dan.sosedoff@gmail.com>
2012-11-08 00:43:46 -0600

    Remove rack test method from spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,5 @@
 $:.unshift File.expand_path("../..", __FILE__)
 
-RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
-end
-
 def fixture_path(filename=nil)
   path = File.expand_path("../fixtures", __FILE__)
   filename.nil? ? path : File.join(path, filename)
