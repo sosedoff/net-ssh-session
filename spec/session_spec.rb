e1521ba623bf9f67208ced8d0e868f567d207b1e
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-04-10 20:35:19 -0500

    Tweak #method_missing spec

--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -18,10 +18,10 @@ describe Net::SSH::Session do
 
   describe '#method_missing' do
     let(:session) { Net::SSH::Session.new('host', 'user', 'password') }
+    before { session.stub(:run).with("uname").and_return(fake_run("uname", "Linux")) }
 
     it 'runs a command based on missing method name' do
-      session.stub(:run).with("uname").and_return(fake_run("uname", "Linux"))
-      session.uname.output.should eq("Linux")
+      expect(session.uname.output).to eq "Linux"
     end
   end
 end
